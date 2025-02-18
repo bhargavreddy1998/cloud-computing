437e93c63981ec5e563ef4fcd7f4a0312e372a9f
Bhargav Reddy Vangala <151866867+bhargavreddy1998@users.noreply.github.com>
2025-02-17 23:26:51 -0700

    Update server.py

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def upload_image_to_s3(file, filename):
 
 def populate_simpledb():
     sdb_client.create_domain(DomainName=SDB_DOMAIN_NAME)
-    with open('Project-1-part-1/CSE546-SPRING-2025/Classification Results on Face Dataset (1000 images).csv', 'r', newline='') as csv_file:
+    with open('/Classification Results on Face Dataset (1000 images).csv', 'r', newline='') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
             if not row:
